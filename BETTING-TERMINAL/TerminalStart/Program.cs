a85d1b92073ee1d96e4e51067a2f093a043f875c
Tigran Vardanyan <vardanyant.89@gmail.com>
2017-02-21 11:18:06 +0400

    coments added

--- BETTING-TERMINAL/TerminalStart/Program.cs
+++ BETTING-TERMINAL/TerminalStart/Program.cs
@@ -8,39 +8,42 @@ namespace TerminalStart
         static void Main(string[] args)
         {
             
-            Terminal.Registration("Sashik", "Sargsyan", new DateTime(2010, 10, 15), Currency.AMD);
-            Terminal.Registration("Robert", "Qocharyan", new DateTime(1995, 1, 5), Currency.USD);
-            // passwords must be sended to emails, or messaged to phone number
+            Terminal.Registration("Sashik", "Sargsyan", new DateTime(1960, 10, 15), Currency.AMD);
+            Terminal.Registration("Robert", "Qocharyan", new DateTime(1955, 1, 5), Currency.USD);
+            // passwords must be sended to emails, or messaged to phone number(not complited)
+
             Console.WriteLine("Enter name and password to Sign in");
             Console.Write("Name - ");
             string name1 = Console.ReadLine();
             Console.Write("Password - ");
             string password1 = Console.ReadLine();
 
+            // Signing In
             Terminal.SignIn(name1, password1);
-
+            // Signing out
             Terminal.SignOut();
+
+            // Trying to add money without signing in(not permited)
             Terminal.AddMoney(new Money(100, Currency.AMD));
 
-
+            // Signing In again
             Terminal.SignIn(name1, password1);
 
+            // Account balance checking
             Terminal.ShowBallance();
 
+            // Adding money(100AMD) to account balance, will show current balance
             Terminal.AddMoney(new Money(100, Currency.AMD));
 
+            // betting 80AMD
             Terminal.Bet(new Money(80, Currency.AMD));
 
+            // betting 50AMD, (not permited, no enough money)
             Terminal.Bet(new Money(50, Currency.AMD));
 
+            // Again addding and betting
             Terminal.AddMoney(new Money(100, Currency.AMD));
-
             Terminal.Bet(new Money(50, Currency.AMD));
-
-
-
-
-
 
             Console.ReadKey();
         }
